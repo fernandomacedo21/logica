b62329b926e2b4a48139bab83530bf48d0511572
Fernando Macedo <fernando.macedo.santana@gmail.com>
2020-06-14 17:40:20 -0300

    Exercicio Quatro- if e else

---
+++ src/ExercicioQuatro/ExercicioQuatro.java
@@ -0,0 +1,31 @@
+package ExercicioQuatro;
+
+import java.util.Scanner;
+
+public class ExercicioQuatro {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("digite seu nome");
+        String nome = ler.nextLine();
+
+        System.out.println("digite sua idade");
+        int idade = ler.nextInt();
+
+
+        if (idade<18){
+            System.out.println(nome + " você é menor de idade ");
+        }else {
+            System.out.println(nome + " você é maior de idade ");
+        }
+
+
+
+
+
+
+
+
+
+    }}
