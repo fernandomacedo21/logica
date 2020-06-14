f153e4f638895e5278fefef9b8f7adb03782603e
Fernando Macedo <fernando.macedo.santana@gmail.com>
2020-06-14 14:53:19 -0300

    exercicio2-operadores logicos e de comparação

---
+++ src/exercicio2/ExercicioDois.java
@@ -0,0 +1,48 @@
+package exercicio2;
+
+public class ExercicioDois {
+    public static void main(String[] args) {
+        //Para operadores E usamos &&
+        System.out.println("O resultado de false e false é "+(false && false));
+        System.out.println(false && true);
+        System.out.println(true && true);
+
+        System.out.println("");
+        System.out.println("");
+
+        //Para operadores OU usamos ||
+        System.out.println(false || false);
+        System.out.println(false || true);
+        System.out.println(true || true);
+
+        System.out.println("");
+        System.out.println("");
+
+        //operadores relacionais
+
+        //variaveis para exemplo
+        int numero1=10;
+        int numero2=5;
+
+        //Para indicar se uma variavel é maior que o outra usamos > , exemplo
+        System.out.println(numero1>numero2);
+
+        //para indicar se uma variável é maior ou igual a outra usamos >=, exemplo:
+        System.out.println(numero1>=numero2);
+
+        //para indicar se uma variavel é menor que outra usamos < exemplo:
+        System.out.println(numero1<numero2);
+
+        //para indicar se uma variável é menor ou igual a outra usamos <= exemplo:
+        System.out.println(numero1<=numero2);
+
+
+        System.out.println("");
+        System.out.println("");
+
+
+        
+
+
+    }
+}
