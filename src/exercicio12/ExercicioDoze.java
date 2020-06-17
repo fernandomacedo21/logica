6fe82142030f0725aff2ee9cf2578196564d6330
Fernando Macedo <fernando.macedo.santana@gmail.com>
2020-06-17 18:14:04 -0300

    Exercicio 12- Praticas de if e else com varias identações

---
+++ src/exercicio12/ExercicioDoze.java
@@ -0,0 +1,40 @@
+package exercicio12;
+
+import java.util.Scanner;
+
+public class ExercicioDoze {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite sua altura: ");
+        int altura =ler.nextInt();
+
+        System.out.println("Digite seu peso: ");
+        int peso = ler.nextInt();
+
+        int imc = peso / (altura*altura);
+        System.out.println("IMC: "+ imc);
+
+        if (imc < 16 ) {
+            System.out.println("Muito abaixo do peso ");
+        } else if (imc >= 17 && imc <18.5) {
+            System.out.println("Abaixo do peso");
+        } else if (imc >=18.5 && imc < 25){
+            System.out.println("Peso Ideal");
+        } else if (imc >=25 && imc < 30){
+            System.out.println(" Sobre Peso ");
+        } else if (imc >=30 && imc <35){
+            System.out.println("Obesidade");
+        } else if (imc >=35 && imc <40){
+            System.out.println("Obesidade severa");
+        } else {
+            System.out.println("Obesidade Morbida");
+        }
+
+
+
+
+
+
+    }
+}
